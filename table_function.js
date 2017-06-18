72c70578796adf6d96d9979efb7f66392dcff4e1
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-18 17:56:23 +0300

    second button still not working

--- table_function.js
+++ table_function.js
@@ -1,28 +1,29 @@
 	function random_spalvos() {
 	var x = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
 	var y = x[Math.floor(Math.random()*x.length)]; // duoda bet kurią x reikšmę
-	 for (i = 0; i < x.length; i++) {
-	 	x[i].addEventListener("click", backToRed())
-	 	function backToRed() {
-	 		x[i].style.backgroundColor = "red";
-	 	} // visi langeliai grįžta į raudoną spalvą
-	 } 
+			 for (i = 0; i < x.length; i++) {
+			 	x[i].addEventListener("click", backToRed())
+					 	function backToRed() {
+					 		x[i].style.backgroundColor = "red";
+					 	} // visi langeliai grįžta į raudoną spalvą
+			 } 
 	y.style.backgroundColor = "white"; // parenka bet kurį langelį ir pakeičia į baltą
 	
 	}
 
 	
 	function iseiles_spalvos() {
-	var x = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
-	var y = x[0];
-	 for (i = 0; i < x.length; i++) {
-	 	x[i].addEventListener("click", backToRed())
-	 	function backToRed() {
-	 		x[i].style.backgroundColor = "red";
-	 	} // visi langeliai grįžta į raudoną spalvą
+	var p = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
+	var z = 0;
+	var r = p[z];
+	
+			for (i = 0; i < x.length; i++) {
+			 	p[i].addEventListener("click", backToRed())
+					 	function backToRed() {
+					 		p[i].style.backgroundColor = "red";
+					 	} // visi langeliai grįžta į raudoną spalvą
+	
+			 } 
+	r.style.backgroundColor = "white";
 
-	 y.style.backgroundColor = "white";
-	 } 
-	
-	
 	}
