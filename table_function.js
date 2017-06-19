0d73c12162c06ca8163c35f5cac8b5088086f5c7
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-19 19:12:55 +0300

    ats for table javaScript
    cells id should be number

--- table_function.js
+++ table_function.js
@@ -1,29 +1,57 @@
-	function random_spalvos() {
-	var x = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
-	var y = x[Math.floor(Math.random()*x.length)]; // duoda bet kurią x reikšmę
-			 for (i = 0; i < x.length; i++) {
-			 	x[i].addEventListener("click", backToRed())
-					 	function backToRed() {
-					 		x[i].style.backgroundColor = "red";
-					 	} // visi langeliai grįžta į raudoną spalvą
-			 } 
-	y.style.backgroundColor = "white"; // parenka bet kurį langelį ir pakeičia į baltą
 	
-	}
+
+
+//	function random_spalvos() {
+//	var x = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
+//	var y = x[Math.floor(Math.random()*x.length)]; // duoda bet kurią x reikšmę
+//			 for (i = 0; i < x.length; i++) {
+//			 	x[i].addEventListener("click", backToRed())
+//					 	function backToRed() {
+//					 		x[i].style.backgroundColor = "red";
+//					 	} // visi langeliai grįžta į raudoną spalvą
+//			 } 
+//	y.style.backgroundColor = "white"; // parenka bet kurį langelį ir pakeičia į baltą
+	
+//	}
 
 	
-	function iseiles_spalvos() {
-	var p = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
-	var z = 0;
-	var r = p[z];
+//	function iseiles_spalvos() {
+//	var p = [document.getElementById("cell_1"), document.getElementById("cell_2"), document.getElementById("cell_3"), document.getElementById("cell_4"), document.getElementById("cell_5"), document.getElementById("cell_6"), document.getElementById("cell_7"), document.getElementById("cell_8"), document.getElementById("cell_9")];
+//	var z = 0;
+//	var r = p[z];
 	
-			for (i = 0; i < x.length; i++) {
-			 	p[i].addEventListener("click", backToRed())
-					 	function backToRed() {
-					 		p[i].style.backgroundColor = "red";
-					 	} // visi langeliai grįžta į raudoną spalvą
+//			for (i = 0; i < x.length; i++) {
+//			 	p[i].addEventListener("click", backToRed())
+//					 	function backToRed() {
+//					 		p[i].style.backgroundColor = "red";
+//					 	} // visi langeliai grįžta į raudoną spalvą
 	
-			 } 
-	r.style.backgroundColor = "white";
+//			 } 
+//	r.style.backgroundColor = "white";
 
+//	}
+// cell id should be number 1-9
+
+var cell = 0;
+function random_spalvos() {
+	 cell = Math.floor(Math.random()*9) +1;
+
+	 for (a = 1; a <= 9; ++a) {
+	 	document.getElementById(a).style.backgroundColor = "red";
+	 } 
+	 document.getElementById(cell).style.backgroundColor = "white";
+
+}
+
+function iseiles_spalvos() {
+	if(cell >= 9) {
+		cel = 1;
+	} else {
+		cell = cell + 1;
 	}
+	for (a = 1; a <= 9; ++a) {
+	 	document.getElementById(a).style.backgroundColor = "red";
+	 } 
+	 document.getElementById(cell).style.backgroundColor = "white";
+
+}
