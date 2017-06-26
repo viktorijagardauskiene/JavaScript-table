617fc68e39ba482c0e2d6e8757c91db1375e04e0
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-26 21:39:16 +0300

    zaidimas veikia

--- table_function.js
+++ table_function.js
@@ -1,42 +1,50 @@
 // cell id should be number 1-9
 
+var SE = [];
+var cell = 0; /* kintamasis cell rasomas virs funkciju kad galiotu abiem, 
+o ne tik vienai. parasyta viduje funkcijos veiks tik toje funkcijoje.*/
+
 function naujas_zaidimas() {
+	SE = [];
+	cell = 0; 
 		for (a = 1; a <= 9; ++a) {
-	 	document.getElementById(a).style.backgroundColor = "red";
-	 } 
+		 	document.getElementById(a).style.backgroundColor = "red";
+		 } 
 		document.getElementById("numeris").style.backgroundColor = "white";
 		document.getElementById("sarasas").style.backgroundColor = "white";
 		document.getElementById("sarasas").innerHTML = "Išrinktų skaičių nėra";
 		document.getElementById("numeris").innerHTML = "Spausk 'Išrink skaičių'";
 
 	var skaiciai = '123456789'; 
-	var bilieto_nr = 0;
-	for (var i = 0; i <7; i++) {
-		bilieto_nr = skaiciai[Math.floor(Math.random()*9)];
-		}
+	var bilieto_nr = [];
+	for (var i = 0; i <6; i++) {
+		bilieto_nr[i] = skaiciai[Math.floor(Math.random()*9)];
+		}	
+		document.getElementById("bilieto_numeris").innerHTML = bilieto_nr;
 	return bilieto_nr;
-	  	document.getElementById("bilieto_numeris").innerHTML = bilieto_nr;
+	  
 	 }
 	 
-var SE = [];
-var cell = 0; /* kintamasis cell rasomas virs funkciju kad galiotu abiem, 
-o ne tik vienai. parasyta viduje funkcijos veiks tik toje funkcijoje.*/
+
 
 function random_spalvos() {
-	 cell = Math.floor(Math.random()*8) +1;
-
-	 for (a = 1; a <= 9; ++a) {
-	 	document.getElementById(a).style.backgroundColor = "red";
-	 } 
-	 var numeris = document.getElementById("numeris");
-	 document.getElementById(cell).style.backgroundColor = random_color();
-	 numeris.innerHTML = cell;
-	 numeris.style.backgroundColor = random_color();
-
+	console.log(SE.length);
+	
 	 if (SE.length <=5) {
-	 SE.push(cell);			/* prideda isrinkta sakiciu prie eiles galo */
-	 sarasas.innerHTML = SE;} else {
-	 	document.getElementById("numeris").innerHTML = "Žaidimas baigėsi"};
+	 	cell = Math.floor(Math.random()*8) +1;
+
+		 for (a = 1; a <= 9; ++a) {
+		 	document.getElementById(a).style.backgroundColor = "red";
+		 } 
+		 var numeris = document.getElementById("numeris");
+		 document.getElementById(cell).style.backgroundColor = random_color();
+		 numeris.innerHTML = cell;
+		 numeris.style.backgroundColor = random_color();
+		 SE.push(cell);			/* prideda isrinkta sakiciu prie eiles galo */
+		 sarasas.innerHTML = SE;
+	} else {
+	 	document.getElementById("numeris").innerHTML = "Žaidimas baigėsi";
+	 };
 
 }
 
